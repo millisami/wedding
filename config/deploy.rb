4c5ed09b3fc7a60b2c206d38ccce5cdb65973bde
Millisami <millisami@gmail.com>
2008-10-29 03:06:45 +0545

    still figuring out deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,103 @@
+default_run_options[:pty] = true
+
+set :application, "handmad-weddingcards.com"
+set :repository,  "git@github.com:millisami/weddingcards.git"
+
+set :deploy_to, "/home/deploy/#{application}"
+set :user, "deploy"
+
+set :scm, :git
+
+role :app, "handmad-weddingcards.com"
+role :web, "handmad-weddingcards.com"
+role :db,  "handmad-weddingcards.com", :primary => true
+
+namespace :slicehost do
+     desc "Update apt-get sources"
+    task :update_apt_get do
+      sudo "apt-get update"
+    end
+
+    desc "Install Development Tools"
+    task :install_dev_tools do
+      sudo "apt-get install build-essential -y"
+    end
+
+    desc "Install Git"
+    task :install_git do
+      sudo "apt-get install git-core git-svn -y"
+    end
+
+    desc "Install SQLite3"
+    task :install_sqlite3 do
+      sudo "apt-get install sqlite3 libsqlite3-ruby -y"
+    end
+
+    desc "Install Ruby, Gems, and Rails"
+    task :install_rails_stack do
+      [
+	"sudo apt-get install ruby ruby1.8-dev irb ri rdoc libopenssl-ruby1.8 -y",
+	"mkdir -p src",
+	"cd src",
+	"wget http://rubyforge.org/frs/download.php/29548/rubygems-1.0.1.tgz",
+	"tar xvzf rubygems-1.0.1.tgz",
+	"cd rubygems-1.0.1/ && sudo ruby setup.rb",
+	"sudo ln -s /usr/bin/gem1.8 /usr/bin/gem",
+	"sudo gem install rails --no-ri --no-rdoc"
+      ].each {|cmd| run cmd}
+    end
+
+    desc "Install Apache"
+    task :install_apache do
+      sudo "apt-get install apache2 apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 apache2-prefork-dev libapr1-dev -y"
+    end
+
+    desc "Install Passenger"
+    task :install_passenger do
+      run "sudo gem install passenger --no-ri --no-rdoc"
+      input = ''
+      run "sudo passenger-install-apache2-module" do |ch,stream,out|
+	next if out.chomp == input.chomp || out.chomp == ''
+	print out
+	ch.send_data(input = $stdin.gets) if out =~ /enter/i
+      end
+    end
+
+    desc "Configure Passenger"
+    task :config_passenger do
+      passenger_config =<<-EOF
+    LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-1.0.1/ext/apache2/mod_passenger.so
+    RailsSpawnServer /usr/lib/ruby/gems/1.8/gems/passenger-1.0.1/bin/passenger-spawn-server
+    RailsRuby /usr/bin/ruby1.8
+      EOF
+      put passenger_config, "src/passenger"
+      sudo "mv src/passenger /etc/apache2/conf.d/passenger"
+    end
+
+    desc "Configure VHost"
+    task :config_vhost do
+      vhost_config =<<-EOF
+    <VirtualHost *:80>
+    ServerName handmade-weddingcards.com
+    DocumentRoot #{deploy_to}/public
+    </VirtualHost>
+      EOF
+      put vhost_config, "src/vhost_config"
+      sudo "mv src/vhost_config /etc/apache2/sites-available/#{application}"
+      sudo "a2ensite #{application}"
+    end
+
+
+end
+
+#==================================================================
 set :application,  "handmade-weddingcards.com"
 
 set :user, "deploy"
 set :runner, user
 #set :use_sudo, true
-
-#set :spinner, false
+#fixing password prompt issue
+#set :sudo, 'sudo -p Password:'
 
 #from #http://github.com/guides/deploying-with-capistrano
 default_run_options[:pty] = true
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Be sure to restart your server when you modify this file
 
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
 
@@ -13,31 +9,13 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.active_record.observers = :user_observer
 
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
-
-  # Skip frameworks you're not going to use (only works if using vendor/rails).
-  # To use Rails without a database, you must remove the Active Record framework
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Only load the plugins named here, in the order given. By default, all plugins 
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
   # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
+  
   config.action_controller.session = {
     :session_key => '_handmadeweddingcards_session',
     :secret      => '8287cf5742b553b8e31b5ac00e6c078319c559e7908d857ff286a80e26a8753fab194004b602ba3dfbb78fdd7f6385c35187d47a5b76f49fd672a0fb7a6fe944'
@@ -48,20 +26,17 @@ Rails::Initializer.run do |config|
   # (create the session table with 'rake db:sessions:create')
   config.action_controller.session_store = :active_record_store
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
-
+  
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
   
 	#-- Custom by Millisami
 	config.active_record.colorize_logging = false
 	config.log_level = :debug
+
+    #gems dependencies
+    config.gem 'RedCloth', :version => '>= 3.0.4', :source => 'http://code.whytheluckystiff.net'
+    config.gem 'aws-s3', :lib => 'aws/s3' #use this syntax for installing gems from github.com
   
   #ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
 	#"<span class='field_error'>#{html_tag}</span>"
