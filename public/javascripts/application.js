0433d621fbb161f89f14dbf205f814eb2481536d
Millisami <millisami@gmail.com>
2008-12-17 12:13:06 +0545

    Removed the sFhover js func from application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,18 +15,6 @@ Ajax.Responders.register({
 function set_class_name(element, class_name) {
 	new Element.ClassNames(element).set(class_name);
 }
-sfHover = function() {
-	var sfEls = document.getElementById("nav").getElementsByTagName("LI");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
-	}
-}
-if (window.attachEvent) window.attachEvent("onload", sfHover); 
 /*jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
 })*/
