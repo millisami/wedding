c7ab5ebd2948ac8a7dd25ca2e1e97a1507646262
millisami <millisami@gmail.com>
2009-01-20 12:48:32 +0545

    Generate the AddPurchasedAtToOrders migration to add purchased_at column in Orders model

---
+++ db/migrate/20090120070054_add_purchased_at_to_orders.rb
@@ -0,0 +1,9 @@
+class AddPurchasedAtToOrders < ActiveRecord::Migration
+  def self.up
+    add_column :orders, :purchased_at, :datetime
+  end
+
+  def self.down
+    remove_column :orders, :purchased_at
+  end
+end
