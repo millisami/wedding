6f4fd7667903cdd0433c66213110f85e54c5fa1e
millisami <millisami@gmail.com>
2009-01-23 12:16:47 +0545

    Integrated the paypal notification in the order checkout process

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,7 +1,7 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
-    PaymentNotification.create!(:params => params, :order_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
+    PaymentNotification.create!(:params => params, :order_id => params[:custom].to_i, :invoice_number => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
     #PaymentNotification.create!(:params => params)
     render :nothing => true
   end
