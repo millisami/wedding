0267c3ca7c2c7e6220c67ac5464561df35cf9560
millisami <millisami@gmail.com>
2009-02-19 15:28:39 +0545

    Direct link in the home category page and edited the partials multiple times rendering of the products list

--- app/controllers/product_sets_controller.rb
+++ app/controllers/product_sets_controller.rb
@@ -1,25 +1,17 @@
 class ProductSetsController < ApplicationController
+    layout 'site'
 	before_filter :find_category
 
   def index
 	@product_sets = @category.product_sets.find(:all)
-	@products = Product.find(:all, :conditions => ["product_set_id = ?", 5]) #@product_sets[0].id])
+
+	@products = Product.find(:all, :conditions => ["product_set_id = ?", @product_sets.first ], :limit => 6)
     respond_to do |format|
-      format.html  { render :layout => false }# index.html.erb
+      format.html
       # format.js
     end
   end
-  
-    def show
-    @product_set = ProductSet.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product_set }
-	  format.js
-    end
-  end
-  
   private #----------------------
   def find_category
 	@category = Category.find(params[:category_id])
