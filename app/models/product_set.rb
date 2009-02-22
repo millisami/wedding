5eda9bbce8b8100a9d4a1b28e0eff2cdfce34255
millisami <millisami@gmail.com>
2009-02-22 22:52:54 +0545

    Deleted the creat_slugs.rb migration and comment out the product_set model

--- app/models/product_set.rb
+++ app/models/product_set.rb
@@ -17,6 +17,6 @@ class ProductSet < ActiveRecord::Base
 	
 	validates_presence_of :name, :message => 'ProductSet name cannot be blank'
 	validates_length_of :name, :within => 3..255
-	has_friendly_id :name, :use_slug => true
+	#has_friendly_id :name, :use_slug => true
 	
 end
