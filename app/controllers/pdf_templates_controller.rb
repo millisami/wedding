f46cf29ac8ac9c19a35db3c1ad670b3e08bcf243
millisami <millisami@gmail.com>
2009-03-08 10:52:22 +0545

    Removed the java iText pdf stamper with the PDF XFA form for the show action

--- app/controllers/pdf_templates_controller.rb
+++ app/controllers/pdf_templates_controller.rb
@@ -1,30 +1,19 @@
 class PdfTemplatesController < ApplicationController
     protect_from_forgery :except => [:parse]
     layout nil
-    #include PdfStamper
-    require 'fileutils'
-    require 'rjb'
-    require 'tmpdir'
-    include FileUtils
-    load_path = File.join(RAILS_ROOT, 'lib', 'iText-2.1.4.jar')
-    options = ['-Djava.awt.headless=true']
-    # RJB needs the LD_LIBRARY_PATH and JAVA_HOME environment set for it to work correctly.  For example on my system:
-    # export LD_LIBRARY_PATH=/usr/java/jdk1.6.0/jre/lib/i386/:/usr/java/jdk1.6.0/jre/lib/i386/client/:./
-    # export JAVA_HOME=/usr/java/jdk1.6.0/
-
-    #ENV['JAVA_HOME'] = 'C:\Program Files\Java\jdk1.6.0_10' if ENV['JAVA_HOME'].nil? and RUBYPLATFORM =~ /mswin32/
-    ENV['JAVA_HOME'] = 'C:\Program Files\Java\jdk1.6.0_10' if ENV['JAVA_HOME'].nil?
-    Rjb::load load_path, options
+    
 
     def show
         @product_details = Product.find(params[:id])
-        #PUT THIS PRODUCT ID IN THE SESSINO AS NEW_PRODUCT_ID_IN_SESSION
-        
-        template(@product_details.document.path)
-      
-        text(:user_id, 13)
-        text(:product_id, params[:id])
-        @pdf_stamper.close
+        @xml_file = File.join(RAILS_ROOT, 'tmp', 'output-pdf-xml-file' + rand(1000000).to_s + '.xml')
+        @output_pdf_file = File.join(RAILS_ROOT, 'tmp', 'output-pdf-file-' + rand(1000000).to_s + '.pdf')
+        doc = Hpricot.XML(File.read(@product_details.pdf_xml.path))
+        #(doc/"product_id").remove
+        @xml_file = @product_details.pdf_xml.path
+        sys_command = "cmd.exe /c #{RAILS_ROOT}/lib/Pravat.exe -i=#{@product_details.document.path} -x=#{@xml_file} -d=#{@output_pdf_file}"
+        debugger
+        system(sys_command)
+
         send_file_options = {
             :type => 'application/pdf', #@product_details.document.content_type,
             #:length => @product_details.document.size,
@@ -32,9 +21,9 @@ class PdfTemplatesController < ApplicationController
             :status => "200 OK"
         }
         if RAILS_ENV == "production"
-            send_file(@tmp_path, send_file_options, :x_sendfile => true) and return
+            send_file(@output_pdf_file, send_file_options, :x_sendfile => true) and return
         else
-            send_file(@tmp_path, send_file_options) and return
+            send_file(@output_pdf_file, send_file_options) and return
         end
     end
 
@@ -94,23 +83,4 @@ class PdfTemplatesController < ApplicationController
             end
 		end
     end
-
-
-    def template(pdf = nil, options = {})
-        filestream   = Rjb::import('java.io.FileOutputStream')
-        pdfreader    = Rjb::import('com.lowagie.text.pdf.PdfReader')
-        pdfstamper   = Rjb::import('com.lowagie.text.pdf.PdfStamper')
-
-        pdf_reader = pdfreader.new(pdf)
-        @tmp_path = File.join(RAILS_ROOT, 'tmp', 'pdf-stamper-' + rand(10000).to_s + '.pdf')
-        file_output_stream = filestream.new(@tmp_path)
-
-        @pdf_stamper = pdfstamper.new(pdf_reader, file_output_stream)
-        @form = @pdf_stamper.getAcroFields()
-    end
-
-    def text(key, value)
-        @form.setField(key.to_s, value.to_s) # Value must be a string or itext will error.
-    end
-
 end
