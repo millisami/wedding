b98b8d0dd51163558473fe36caf2f09e67316b87
millisami <millisami@gmail.com>
2009-03-13 13:46:33 +0545

    fixing

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,10 @@
 module ProductsHelper
+#    def is_pdf_customized(product_id)
+#        existing_item = @cart.items.find {|item|item.product_id == product_id}
+#        if existing_item.pdf_xml_data
+#            return true
+#        else
+#            return false
+#        end
+#    end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,14 @@ class Cart
 	def initialize
 		empty_all_items
 	end
-
+    def is_pdf_customized(product_id)
+        existing_item = @items.find {|item|item.product_id == product_id}
+        if existing_item && existing_item.pdf_xml_data
+            return true
+        else
+            return false
+        end
+    end
     def add_pdf_xml_data(xml_data, session_id)
         doc = Hpricot.XML(xml_data)
         product_id = (doc/"product_id").inner_html.to_i
