7211f375393cecb426478db0d2a079ce3503538a
millisami <millisami@gmail.com>
2009-03-21 22:09:04 +0545

    Altered Paypal payment to use encrypted form, but paypal error still exists

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -5,7 +5,7 @@ class PaymentNotification < ActiveRecord::Base
 
   private
   def mark_order_as_purchased
-    if status == "Completed"
+    if status == "Completed" && params[:secret] == APP_CONFIG[:paypal_secret] && params[:receiver_email] == APP_CONFIG[:paypal_email]
       order.update_attribute(:purchased_at, Time.now)
       #@cart.empty_all_items
     end
