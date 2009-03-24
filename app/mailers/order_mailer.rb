a4d4a515e361442a2600c7e509c8b464fcec681d
millisami <millisami@gmail.com>
2009-03-24 23:11:57 +0545

    Added Order mailer layouts and made the email content-type to html

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,12 +1,13 @@
 class OrderMailer < ApplicationMailer
-
+    layout 'order_mailer'
     def order_email(order)
         recipients   order.email
         from          "info@handmade-weddingcards.com" #can use APP_CONFIG[...] to make it customizable
         subject      "Order Placement (Order ID: #{order.id}) - Handmade Wedding Cards"
         sent_on      Time.now
         bcc          "Bjorn <bjorn@fairenterprise.net>, Watabaran <info@watabaran.org>"
-        body    :order => order
+        body        :order => order
+        content_type "text/html"
+        
     end
-
 end
